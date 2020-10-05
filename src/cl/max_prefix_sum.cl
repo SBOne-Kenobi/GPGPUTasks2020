d9e791d303f9337381445aa91fc75a3730fe6bdd
SBOne-Kenobi <snob-1432e@mail.ru>
2020-10-05 18:58:26 +0300

    very bad prefix

--- src/cl/max_prefix_sum.cl
+++ src/cl/max_prefix_sum.cl
@@ -1,1 +1,55 @@
-// TODO
+#ifdef __CLION_IDE__
+
+#include <libgpu/opencl/cl/clion_defines.cl>
+
+#endif
+
+#ifndef WORK_GROUP_SIZE
+#define WORK_GROUP_SIZE 128
+#endif
+
+#line 6
+
+__kernel void max_prefix(
+        __global int *buffer,
+        int len) {
+  const int localId = get_local_id(0);
+  const int globalId = get_global_id(0);
+  const int groupId = get_group_id(0);
+
+  __local int uploaded[3 * WORK_GROUP_SIZE];
+
+  for (int i = 0; i < 3; i++) {
+    uploaded[i * WORK_GROUP_SIZE + localId] = buffer[3 * groupId * WORK_GROUP_SIZE + i * WORK_GROUP_SIZE + localId];
+  }
+
+  //TODO : optimize
+  barrier(CLK_LOCAL_MEM_FENCE);
+
+  if (globalId < len) {
+    if (localId == 0) {
+      int sum = 0;
+      int res = 0;
+      int max_sum = 0;
+      for (int i = 0; i < min(WORK_GROUP_SIZE, len - globalId); ++i) {
+        int part_sum = uploaded[3 * i + 0];
+        int part_res = uploaded[3 * i + 1];
+        int part_max_sum = uploaded[3 * i + 2] + sum;
+        if (max_sum < part_max_sum)
+          max_sum = part_max_sum, res = part_res;
+        sum += part_sum;
+      }
+      uploaded[0] = sum;
+      uploaded[1] = res;
+      uploaded[2] = max_sum;
+//      printf("---------LOG : global_id=%d, len=%d, ans=(%d, %d, %d)\n", globalId, *len, max_sum, res, sum);
+    }
+  }
+
+  if (localId == 0 && globalId < len) {
+    // replace results back
+    buffer[3 * groupId * WORK_GROUP_SIZE + 0] = uploaded[0];
+    buffer[3 * groupId * WORK_GROUP_SIZE + 1] = uploaded[1];
+    buffer[3 * groupId * WORK_GROUP_SIZE + 2] = uploaded[2];
+  }
+}
--- src/main_max_prefix_sum.cpp
+++ src/main_max_prefix_sum.cpp
@@ -1,77 +1,150 @@
 #include <libutils/misc.h>
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
 
+#include "cl/max_prefix_sum_cl.h"
 
 template<typename T>
-void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
-{
-    if (a != b) {
-        std::cerr << message << " But " << a << " != " << b << ", " << filename << ":" << line << std::endl;
-        throw std::runtime_error(message);
-    }
+void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line) {
+  if (a != b) {
+    std::cerr << message << " But " << a << " != " << b << ", " << filename << ":" << line << std::endl;
+    throw std::runtime_error(message);
+  }
 }
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
-
-int main(int argc, char **argv)
-{
-    int benchmarkingIters = 10;
-    int max_n = (1 << 24);
-
-    for (int n = 2; n <= max_n; n *= 2) {
-        std::cout << "______________________________________________" << std::endl;
-        int values_range = std::min(1023, std::numeric_limits<int>::max() / n);
-        std::cout << "n=" << n << " values in range: [" << (-values_range) << "; " << values_range << "]" << std::endl;
-
-        std::vector<int> as(n, 0);
-        FastRandom r(n);
-        for (int i = 0; i < n; ++i) {
-            as[i] = (unsigned int) r.next(-values_range, values_range);
-        }
-
-        int reference_max_sum;
-        int reference_result;
-        {
-            int max_sum = 0;
-            int sum = 0;
-            int result = 0;
-            for (int i = 0; i < n; ++i) {
-                sum += as[i];
-                if (sum > max_sum) {
-                    max_sum = sum;
-                    result = i + 1;
-                }
-            }
-            reference_max_sum = max_sum;
-            reference_result = result;
-        }
-        std::cout << "Max prefix sum: " << reference_max_sum << " on prefix [0; " << reference_result << ")" << std::endl;
-
-        {
-            timer t;
-            for (int iter = 0; iter < benchmarkingIters; ++iter) {
-                int max_sum = 0;
-                int sum = 0;
-                int result = 0;
-                for (int i = 0; i < n; ++i) {
-                    sum += as[i];
-                    if (sum > max_sum) {
-                        max_sum = sum;
-                        result = i + 1;
-                    }
-                }
-                EXPECT_THE_SAME(reference_max_sum, max_sum, "CPU result should be consistent!");
-                EXPECT_THE_SAME(reference_result, result, "CPU result should be consistent!");
-                t.nextLap();
-            }
-            std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-            std::cout << "CPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
-        }
-
-        {
-            // TODO: implement on OpenCL
-        }
+void init_buffer(
+        int *buffer,
+        const int *a,
+        int n, int workSize) {
+  for (int i = 0; i < workSize; i++) {
+    if (i < n) {
+      buffer[3 * i + 0] = a[i];
+      if (a[i] > 0) {
+        buffer[3 * i + 1] = i + 1;
+        buffer[3 * i + 2] = a[i];
+      } else {
+        buffer[3 * i + 1] = i;
+        buffer[3 * i + 2] = 0;
+      }
+    } else {
+      buffer[3 * i + 0] = 0;
+      buffer[3 * i + 1] = i;
+      buffer[3 * i + 2] = 0;
     }
+  }
+}
+
+void reorder_buffer(int *buffer, int len, int groupSize) {
+  int cnt = (len + groupSize - 1) / groupSize;
+  for (int i = 1; i < cnt; ++i) {
+    buffer[3 * i + 0] = buffer[3 * (i * groupSize) + 0];
+    buffer[3 * i + 1] = buffer[3 * (i * groupSize) + 1];
+    buffer[3 * i + 2] = buffer[3 * (i * groupSize) + 2];
+  }
+}
+
+int main(int argc, char **argv) {
+  gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+  gpu::Context context;
+  context.init(device.device_id_opencl);
+  context.activate();
+
+  size_t groupSize = 128;
+
+  std::string defines = " -D WORK_GROUP_SIZE=" + to_string(groupSize);
+  ocl::Kernel kernel(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "max_prefix", defines);
+  kernel.compile();
+
+  gpu::gpu_mem_32i buffer_gpu;
+
+  int benchmarkingIters = 1;
+  int max_n = (1 << 24);
+
+  for (int n = 2; n <= max_n; n *= 2) {
+    std::cout << "______________________________________________" << std::endl;
+    int values_range = std::min(1023, std::numeric_limits<int>::max() / n);
+    std::cout << "n=" << n << " values in range: [" << (-values_range) << "; " << values_range << "]" << std::endl;
+
+    std::vector<int> as(n, 0);
+    FastRandom r(n);
+    for (int i = 0; i < n; ++i) {
+      as[i] = (unsigned int) r.next(-values_range, values_range);
+    }
+
+    int reference_max_sum;
+    int reference_result;
+    {
+      int max_sum = 0;
+      int sum = 0;
+      int result = 0;
+      for (int i = 0; i < n; ++i) {
+        sum += as[i];
+        if (sum > max_sum) {
+          max_sum = sum;
+          result = i + 1;
+        }
+      }
+      reference_max_sum = max_sum;
+      reference_result = result;
+    }
+    std::cout << "Max prefix sum: " << reference_max_sum << " on prefix [0; " << reference_result << ")" << std::endl;
+
+    {
+      timer t;
+      for (int iter = 0; iter < benchmarkingIters; ++iter) {
+        int max_sum = 0;
+        int sum = 0;
+        int result = 0;
+        for (int i = 0; i < n; ++i) {
+          sum += as[i];
+          if (sum > max_sum) {
+            max_sum = sum;
+            result = i + 1;
+          }
+        }
+        EXPECT_THE_SAME(reference_max_sum, max_sum, "CPU result should be consistent!");
+        EXPECT_THE_SAME(reference_result, result, "CPU result should be consistent!");
+        t.nextLap();
+      }
+      std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+      std::cout << "CPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
+    }
+
+    {
+      size_t globalSize = (n + groupSize - 1) / groupSize * groupSize;
+      std::vector<int> buffer(3 * globalSize);
+
+      timer t;
+      for (int i = 0; i < benchmarkingIters; i++) {
+        int max_sum;
+        int res;
+
+        init_buffer(buffer.data(), as.data(), n, globalSize);
+        for (int len = globalSize; len > 1; len /= groupSize) {
+          buffer_gpu.resizeN(3 * len);
+          buffer_gpu.writeN(buffer.data(), 3 * len);
+
+          gpu::WorkSize workSize(groupSize, (len + groupSize - 1) / groupSize * groupSize);
+
+          kernel.exec(workSize, buffer_gpu, len);
+
+          buffer_gpu.readN(buffer.data(), 3 * len);
+          reorder_buffer(buffer.data(), len, groupSize);
+        }
+
+        max_sum = buffer[2];
+        res = buffer[1];
+
+        EXPECT_THE_SAME(reference_max_sum, max_sum, "GPU result should be consistent!");
+        EXPECT_THE_SAME(reference_result, res, "GPU result should be consistent!");
+        t.nextLap();
+      }
+      std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+      std::cout << "GPU: " << (globalSize / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
+    }
+  }
 }
