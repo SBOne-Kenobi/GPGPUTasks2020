46d27725b0461f98531c8187108786277d5bd265
SBOne-Kenobi <snob-1432e@mail.ru>
2020-10-12 18:36:13 +0300

    add transpose

--- src/cl/matrix_transpose.cl
+++ src/cl/matrix_transpose.cl
@@ -1,4 +1,60 @@
-__kernel void matrix_transpose(...)
-{
-    // TODO
+#ifdef __CLION_IDE__
+
+#include <libgpu/opencl/cl/clion_defines.cl>
+
+#endif
+
+#line 6
+
+#ifndef WORK_GROUP_SIZE
+#define WORK_GROUP_SIZE 128
+#endif
+
+#define TILE_SIZE 32
+
+__kernel void matrix_transpose(
+        __global const float *a,
+        __global float *t_a,
+        unsigned int M, unsigned int K
+) {
+  const unsigned int local_id = get_local_id(0);
+  const unsigned int global_id = get_global_id(0);
+  const unsigned int group_id = get_group_id(0);
+  int block_num_x = (K + TILE_SIZE - 1) / TILE_SIZE;
+  int block_x = group_id % block_num_x;
+  int block_y = group_id / block_num_x;
+  int offset_x = block_x * TILE_SIZE;
+  int offset_y = block_y * TILE_SIZE;
+
+  __local float uploaded[TILE_SIZE * (TILE_SIZE + 1)];
+  for (int i = local_id; i < TILE_SIZE * TILE_SIZE; i += WORK_GROUP_SIZE) {
+    int x = i % TILE_SIZE;
+    int y = i / TILE_SIZE;
+    int to = x + y * (TILE_SIZE + 1);
+
+    int cur_x = offset_x + x;
+    int cur_y = offset_y + y;
+    int from = cur_x + cur_y * K;
+
+    if (cur_x < K && cur_y < M) {
+      uploaded[to] = a[from];
+    }
+  }
+
+  barrier(CLK_LOCAL_MEM_FENCE);
+
+  for (int i = local_id; i < TILE_SIZE * TILE_SIZE; i += WORK_GROUP_SIZE) {
+    int y = i % TILE_SIZE;
+    int x = i / TILE_SIZE;
+    int from = x + y * (TILE_SIZE + 1);
+
+    int cur_x = offset_x + x;
+    int cur_y = offset_y + y;
+    int to = cur_x * M + cur_y;
+
+    if (cur_x < K && cur_y < M) {
+      t_a[to] = uploaded[from];
+    }
+  }
+
 }
--- src/main_matrix_transpose.cpp
+++ src/main_matrix_transpose.cpp
@@ -11,65 +11,65 @@
 #include <stdexcept>
 
 
-int main(int argc, char **argv)
-{
-    gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+int main(int argc, char **argv) {
+  gpu::Device device = gpu::chooseGPUDevice(argc, argv);
 
-    gpu::Context context;
-    context.init(device.device_id_opencl);
-    context.activate();
+  gpu::Context context;
+  context.init(device.device_id_opencl);
+  context.activate();
 
-    int benchmarkingIters = 10;
-    unsigned int M = 1024;
-    unsigned int K = 1024;
+  int benchmarkingIters = 10;
+  unsigned int M = 10240;
+  unsigned int K = 10240;
 
-    std::vector<float> as(M*K, 0);
-    std::vector<float> as_t(M*K, 0);
+  std::vector<float> as(M * K, 0);
+  std::vector<float> as_t(M * K, 0);
 
-    FastRandom r(M+K);
-    for (unsigned int i = 0; i < as.size(); ++i) {
-        as[i] = r.nextf();
+  FastRandom r(M + K);
+  for (unsigned int i = 0; i < as.size(); ++i) {
+    as[i] = r.nextf();
+  }
+  std::cout << "Data generated for M=" << M << ", K=" << K << "!" << std::endl;
+
+
+  gpu::gpu_mem_32f as_gpu, as_t_gpu;
+  as_gpu.resizeN(M * K);
+  as_t_gpu.resizeN(K * M);
+
+  as_gpu.writeN(as.data(), M * K);
+  unsigned int work_group_size = 128;
+  std::string defines = " -D WORK_GROUP_SIZE=" + to_string(work_group_size);
+
+  ocl::Kernel matrix_transpose_kernel(matrix_transpose, matrix_transpose_length, "matrix_transpose", defines);
+  matrix_transpose_kernel.compile();
+
+  {
+    timer t;
+    for (int iter = 0; iter < benchmarkingIters; ++iter) {
+      unsigned int global_work_size = (K * M + work_group_size - 1) / work_group_size * work_group_size;
+      gpu::WorkSize workSize(work_group_size, global_work_size);
+
+      matrix_transpose_kernel.exec(workSize, as_gpu, as_t_gpu, M, K);
+
+      t.nextLap();
     }
-    std::cout << "Data generated for M=" << M << ", K=" << K << "!" << std::endl;
+    std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+    std::cout << "GPU: " << M * K / 1000.0 / 1000.0 / t.lapAvg() << " millions/s" << std::endl;
+  }
 
-    /*
-    gpu::gpu_mem_32f as_gpu, as_t_gpu;
-    as_gpu.resizeN(M*K);
-    as_t_gpu.resizeN(K*M);
+  as_t_gpu.readN(as_t.data(), M * K);
 
-    as_gpu.writeN(as.data(), M*K);
-
-    ocl::Kernel matrix_transpose_kernel(matrix_transpose, matrix_transpose_length, "matrix_transpose");
-    matrix_transpose_kernel.compile();
-
-    {
-        timer t;
-        for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            // TODO
-            unsigned int work_group_size = 128;
-            unsigned int global_work_size = ...;
-            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, as_t_gpu, M, K);
-
-            t.nextLap();
-        }
-        std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << M*K/1000.0/1000.0 / t.lapAvg() << " millions/s" << std::endl;
+  // Проверяем корректность результатов
+  for (int j = 0; j < M; ++j) {
+    for (int i = 0; i < K; ++i) {
+      float a = as[j * K + i];
+      float b = as_t[i * M + j];
+      if (a != b) {
+        std::cerr << "Not the same!" << std::endl;
+        return 1;
+      }
     }
+  }
 
-    as_t_gpu.readN(as_t.data(), M*K);
-
-    // Проверяем корректность результатов
-    for (int j = 0; j < M; ++j) {
-        for (int i = 0; i < K; ++i) {
-            float a = as[j * K + i];
-            float b = as_t[i * M + j];
-            if (a != b) {
-                std::cerr << "Not the same!" << std::endl;
-                return 1;
-            }
-        }
-    }
-    */
-
-    return 0;
+  return 0;
 }
